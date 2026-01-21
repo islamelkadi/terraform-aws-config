57c92733f2fbbb7a21ccd31574b2ade9b5a33ca2
Islam Elkadi <153553770+islamelkadi@users.noreply.github.com>
2026-01-21 18:55:12 -0500

    fix: Addressed linter warnings (#2)
    * chore: Remove unused AWS data sources from data.tf
    
    Removed unused data sources for AWS region and caller identity.
    
    * chore: Clean up locals by removing unused variables
    
    Removed unused local variables for region and account ID.
    
    * fix: Add Terraform provider configuration for AWS
    
    * chore: Fix formatting of project_id_prefix in locals.tf

--- modules/aggregator/data.tf
+++ modules/aggregator/data.tf
@@ -1,6 +1,3 @@
-data "aws_region" "current" {}
-data "aws_caller_identity" "current" {}
-
 # Check if the AWS Config service-linked role exists
 data "aws_iam_role" "config_service_role" {
   count = var.organization_aggregator_role_arn == null ? 1 : 0
