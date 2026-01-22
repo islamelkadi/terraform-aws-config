6fd9298a7349065efcf4a679ca3a668521943848
Islam Elkadi <153553770+islamelkadi@users.noreply.github.com>
2026-01-21 19:30:36 -0500

    chore: added examples directory to demonstrate usage (#3)

---
+++ examples/aggregator/account-aggregator/main.tf
@@ -0,0 +1,26 @@
+# Account-level Config Aggregator
+module "config_aggregator" {
+  source = "../../../modules/aggregator"
+
+  # Generic variables
+  environment  = var.environment
+  region       = var.region
+  project_name = var.project_name
+  organization = var.organization
+
+  # Aggregator configuration
+  aggregator_name           = var.aggregator_name
+  enable_all_regions        = var.enable_all_regions
+  aggregator_target_regions = var.aggregator_target_regions
+
+  # Account aggregation (enabled)
+  enable_account_aggregation = var.enable_account_aggregation
+  aggregation_account_ids    = var.aggregation_account_ids
+
+  # Organization aggregation (disabled)
+  enable_organization_aggregation  = var.enable_organization_aggregation
+  organization_aggregator_role_arn = var.organization_aggregator_role_arn
+
+  # Tags
+  additional_tags = var.additional_tags
+}
---
+++ examples/aggregator/organization-aggregator/main.tf
@@ -0,0 +1,26 @@
+# Organization-level Config Aggregator
+module "config_aggregator" {
+  source = "../../../modules/aggregator"
+
+  # Generic variables
+  environment  = var.environment
+  region       = var.region
+  project_name = var.project_name
+  organization = var.organization
+
+  # Aggregator configuration
+  aggregator_name           = var.aggregator_name
+  enable_all_regions        = var.enable_all_regions
+  aggregator_target_regions = var.aggregator_target_regions
+
+  # Organization aggregation (enabled)
+  enable_organization_aggregation  = var.enable_organization_aggregation
+  organization_aggregator_role_arn = var.organization_aggregator_role_arn
+
+  # Account aggregation (disabled)
+  enable_account_aggregation = var.enable_account_aggregation
+  aggregation_account_ids    = var.aggregation_account_ids
+
+  # Tags
+  additional_tags = var.additional_tags
+}
