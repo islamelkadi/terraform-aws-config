0b309d652687b202aac5310b3b3db1bc97ba83c6
Islam Elkadi <153553770+islamelkadi@users.noreply.github.com>
2026-01-21 19:22:56 -0500

    Remove Terraform configuration for AWS provider
    Removed Terraform provider and version requirements.

--- examples/aggregator/account-aggregator/main.tf
+++ examples/aggregator/account-aggregator/main.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_version = ">= 1.14.3"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.28.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.region
-}
-
 # Account-level Config Aggregator
 module "config_aggregator" {
   source = "../../modules/config/aggregator"
