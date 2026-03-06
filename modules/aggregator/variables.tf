20b46de2190e44a08c1b5992cd83607f4a91dd43
Islam Elkadi <153553770+islamelkadi@users.noreply.github.com>
2026-03-06 14:33:09 -0500

    Update variables.tf

--- modules/aggregator/variables.tf
+++ modules/aggregator/variables.tf
@@ -1,68 +1,70 @@
-# Generic module variables
+# Config Aggregator Module Variables
+
+# Metadata variables for consistent naming
+variable "namespace" {
+  description = "Namespace (organization/team name)"
+  type        = string
+}
+
 variable "environment" {
-  description = "The name of the environment to deploy the config aggregator (dev, test, prod, etc). This is used for semantic organization."
+  description = "Environment name (dev, staging, prod)"
+  type        = string
+
+  validation {
+    condition     = contains(["dev", "staging", "prod"], var.environment)
+    error_message = "Environment must be dev, staging, or prod"
+  }
+}
+
+variable "name" {
+  description = "Name of the config aggregator"
   type        = string
 }
 
-variable "region" {
-  description = "The name of the AWS region to deploy the config aggregator."
-  type        = string
-}
-
-variable "project_name" {
-  description = "A unique name to assign for this project. This is used for semantic organization."
-  type        = string
-}
-
-variable "organization" {
-  description = "The name of the organization to deploy the config aggregator. This is used for semantic organization."
-  type        = string
-}
-
-variable "additional_tags" {
-  description = "A map of generic additional tags to blanket apply to resources created via this module."
+variable "tags" {
+  description = "Additional tags to apply to resources"
   type        = map(string)
   default     = {}
 }
 
-# AWS Config Aggregator variables
-variable "aggregator_name" {
-  description = "A unique name to assign to this AWS configuration aggregator."
+variable "region" {
+  description = "AWS region where resources will be created"
   type        = string
 }
 
+# AWS Config Aggregator configuration
 variable "enable_all_regions" {
-  description = "A flag to enable aggregating data from all AWS regions."
+  description = "Enable aggregating data from all AWS regions"
   type        = bool
   default     = true
 }
 
 variable "aggregator_target_regions" {
-  description = "A list of target regions to aggregate config data from (used when all_regions is false)."
+  description = "List of target regions to aggregate config data from (used when enable_all_regions is false)"
   type        = list(string)
   default     = []
 }
 
 variable "enable_account_aggregation" {
-  description = "A flag to enable AWS account(s) aggregation source type."
+  description = "Enable AWS account(s) aggregation source type"
   type        = bool
   default     = false
 }
 
 variable "aggregation_account_ids" {
-  description = "A list of target AWS account IDs to aggregate config data from."
+  description = "List of target AWS account IDs to aggregate config data from"
   type        = list(string)
   default     = []
 }
 
 variable "enable_organization_aggregation" {
-  description = "A flag to enable AWS Organization aggregation source type."
+  description = "Enable AWS Organization aggregation source type"
   type        = bool
   default     = false
 }
 
 variable "organization_aggregator_role_arn" {
-  description = "The IAM role ARN to use to allow AWS Config uses to aggregate config data."
+  description = "IAM role ARN for AWS Config to aggregate config data. Falls back to AWSServiceRoleForConfig if not provided"
   type        = string
   default     = null
 }
