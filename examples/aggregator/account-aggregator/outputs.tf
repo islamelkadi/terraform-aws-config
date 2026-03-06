501612288df54886265c616b95070840fae8f81b
Islam Elkadi <153553770+islamelkadi@users.noreply.github.com>
2026-03-06 14:37:10 -0500

    Delete examples/aggregator directory

--- examples/aggregator/account-aggregator/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "aggregator_arn" {
-  description = "The ARN of the config aggregator"
-  value       = module.config_aggregator.config_aggregator_arn
-}
-
-output "aggregator_id" {
-  description = "The ID of the config aggregator"
-  value       = module.config_aggregator.config_aggregator_id
-}
